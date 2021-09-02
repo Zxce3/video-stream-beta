2a6bf283f9b5da132d155d9c319cd6797febdfa1
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-02 18:56:41 +0700

    setup

--- bot/callback.py
+++ bot/callback.py
@@ -5,12 +5,14 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 async def cbguide(_, query: CallbackQuery):
   await query.edit_message_text(
     f"""❓ HOW TO USE THIS BOT:
+
 1.) first, add me to your group.
 2.) then promote me as admin and give all permissions except anonymous admin.
 3.) add @tg_video_stream to your group.
 4.) turn on the voice chat first before start to stream video.
 5.) type /stream (reply to video) to start streaming.
 6.) type /stop to end the video streaming.
+
 ⚡ __Maintained by Veez Project Team__""",
     reply_markup=InlineKeyboardMarkup(
       [[
@@ -44,12 +46,17 @@ async def cbstart(_, query: CallbackQuery):
 async def cbinfo(_, query: CallbackQuery):
   await query.edit_message_text(
     f"""🌐 **bot information**
+
 🤖 __This bot was created to stream video in telegram group video chats using several methods from WebRTC.__
+
 💡 __Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.__
+
 👨🏻‍💻 __Thanks to the developers who participated in the development of this bot, the list of devs can be seen below:__
+
 👩🏻‍✈️ » [Levina](https://github.com/levina-lab)
 🤵🏻 » [Sammy-XD](https://github.com/Sammy-XD)
 🤵🏻 » [Achu](https://github.com/Achu2234)
+
 __This bot licensed under GNU-GPL 3.0 License__""",
     reply_markup=InlineKeyboardMarkup(
       [[
