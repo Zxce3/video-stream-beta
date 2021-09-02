21e2e1bf82ddf0acfa0e3413eb274203cb1459f7
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-02 16:32:05 +0700

    tgcalls
    commited

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -14,13 +14,13 @@ VIDEO_CALL = {}
 async def stream(client, m: Message):
     replied = m.reply_to_message
     if not replied:
-        await m.reply("`please reply to a video !`")
+        await m.reply("😕 **sorry it's not a video**\n\n» use the /stream command by replying to the video.")
     elif replied.video or replied.document:
-        msg = await m.reply("🔁 `Downloading...`")
+        msg = await m.reply("🔁 **downloading video...**\n\n❔ this process will take quite a while depending on the size of the video.")
         chat_id = m.chat.id
         try:
             video = await client.download_media(m.reply_to_message)
-            await msg.edit("♻ `Converting...`")
+            await msg.edit("♻ **Converting...**")
             os.system(f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" vid-{chat_id}.raw -y')
         except Exception as e:
             await msg.edit(f"**🚫 Error** - `{e}`")
@@ -30,7 +30,7 @@ async def stream(client, m: Message):
             await group_call.start(chat_id)
             await group_call.set_video_capture(video)
             VIDEO_CALL[chat_id] = group_call
-            await msg.edit("**💡 streaming has started !**")
+            await msg.edit("**💡 video streaming started !\n\n❕ join to video chat to watch the video.**")
         except Exception as e:
             await msg.edit(f"**Error** -- `{e}`")
     else:
@@ -41,6 +41,6 @@ async def stopvideo(client, m: Message):
     chat_id = m.chat.id
     try:
         await VIDEO_CALL[chat_id].stop()
-        await m.reply("**⏹️ streaming stopped !\n\n✨ thank you for using this bot.**")
+        await m.reply("**⏹️ streaming has ended !**\n\n✅ __userbot has been disconnected from the video chat__")
     except Exception as e:
         await m.reply(f"**🚫 Error** - `{e}`")
