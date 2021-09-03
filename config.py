45893a052e2e91ac0d5973fa28983d4a2352adb7
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-03 22:17:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-import os
-from os import getenv
+from os import path, getenv
 from dotenv import load_dotenv
 
-if os.path.exists("local.env"):
+if path.exists("local.env"):
     load_dotenv("local.env")
 
 load_dotenv()
 admins = {}
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+API_ID = int(getenv("API_ID", "6"))
+API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION_NAME = getenv("SESSION_NAME", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
