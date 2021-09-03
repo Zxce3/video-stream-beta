f6b6829f8cfb040d5bf196373121fdcaf7696200
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-03 10:17:57 +0700

    added var

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
+admins = {}
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SESSION_NAME = os.getenv("SESSION_NAME")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
