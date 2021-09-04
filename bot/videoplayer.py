4be399f5d8b186ecb510ae2e474ed78db555ef41
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-05 00:27:48 +0700

    using default code for stream
    but the youtube stream function will be added again later

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -1,155 +1,55 @@
 import os
-import time
-import ffmpeg
 import asyncio
-from os import path
-from asyncio import sleep
-from youtube_dl import YoutubeDL
 from pytgcalls import GroupCallFactory
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.errors import FloodWait
-from config import API_ID, API_HASH, SESSION_NAME, BOT_USERNAME, CHAT_ID
-from helpers.decorators import authorized_users_only
-from helpers.filters import command
-
+from config import API_ID, API_HASH, SESSION_NAME
 
 app = Client(SESSION_NAME, API_ID, API_HASH)
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
-
-STREAM = {6}
 VIDEO_CALL = {}
 
-ydl_opts = {
-        "format": "best",
-        "addmetadata": True,
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "videoformat": "mp4",
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-}
-ydl = YoutubeDL(ydl_opts)
 
-@Client.on_message(command(["vstream", f"vstream@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
-@authorized_users_only
-async def vstream(client, m: Message):
-    if 1 in STREAM:
-        await m.reply_text("😕 **sorry, there's another video streaming right now**\n\n» **wait for it to finish then try again!**")
-        return
-    media = m.reply_to_message
-    if not media and not ' ' in m.text:
-        await m.reply("💭 **Give me a video or youtube url to stream**\n\n» Use the /vstream command by replying to the video or youtube url.")
-
-    elif ' ' in m.text:
-        msg = await m.reply_text("📥 **downloading video...**\n\n💭 __this process will take quite a while depending on the size of the video.__")
-        text = m.text.split(' ', 1)
-        url = text[1]
-        if os.path.exists(f'stream-{CHAT_ID}.raw'):
-            os.remove(f'stream-{CHAT_ID}.raw')
+@Client.on_message(filters.command("stream"))
+async def stream(client, m: Message):
+    replied = m.reply_to_message
+    if not replied:
+        if len(m.command) < 2:
+            await m.reply("🔺 **please reply to a video or live stream url to stream video!**")
+        else:
+            video = m.text.split(None, 1)[1]
+            msg = await m.reply("__starting live stream...__")
+            chat_id = m.chat.id
+            await asyncio.sleep(1)
+            try:
+                group_call = group_call_factory.get_group_call()
+                await group_call.join(chat_id)
+                await group_call.start_video(video)
+                VIDEO_CALL[chat_id] = group_call
+                await msg.edit(f"💡 **live streaming started!**\n\n» **join to video chat to watch the live stream.**")
+            except Exception as e:
+                await msg.edit(f"**Error** -- `{e}`")
+    elif replied.video or replied.document:
+        msg = await m.reply("📥 **downloading video...**\n\n💭 __this process will take quite a while depending on the size of the video.__")
+        video = await client.download_media(m.reply_to_message)
+        chat_id = m.chat.id
+        await asyncio.sleep(2)
         try:
-            info = ydl.extract_info(url, False)
-            ydl.download([url])
-            video = path.join("downloads", f"{info['id']}.{info['ext']}")
-            await msg.edit("♻️ **converting video...**")
-            os.system(f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" stream-{CHAT_ID}.raw -y')
-        except Exception as e:
-            await msg.edit(f"❌ **something went wrong...** \n\nError: `{e}`")
-        await sleep(5)
-        group_call = group_call_factory.get_file_group_call(f'stream-{CHAT_ID}.raw')
-        try:
-            await group_call.start(CHAT_ID)
-            await group_call.set_video_capture(video)
-            VIDEO_CALL[CHAT_ID] = group_call
+            group_call = group_call_factory.get_group_call()
+            await group_call.join(chat_id)
+            await group_call.start_video(video)
+            VIDEO_CALL[chat_id] = group_call
             await msg.edit("💡 **video streaming started!**\n\n» **join to video chat to watch the video.**")
-            try:
-                STREAM.remove(0)
-            except:
-                pass
-            try:
-                STREAM.add(1)
-            except:
-                pass
-        except FloodWait as e:
-            await sleep(e.x)
-            if not group_call.is_connected:
-                await group_call.start(CHAT_ID)
-                await group_call.set_video_capture(video)
-                VIDEO_CALL[CHAT_ID] = group_call
-                await msg.edit("💡 **video streaming started!**\n\n» **join to video chat to watch the video.**")
-                try:
-                    STREAM.remove(0)
-                except:
-                    pass
-                try:
-                    STREAM.add(1)
-                except:
-                    pass
         except Exception as e:
-            await msg.edit(f"❌ **something went wrong...** \n\nError: `{e}`")
-            
-    elif media.video or media.document:
-        msg = await m.reply_text("📥 **downloading video...**\n\n💭 __this process will take quite a while depending on the size of the video.__")
-        if os.path.exists(f'stream-{CHAT_ID}.raw'):
-            os.remove(f'stream-{CHAT_ID}.raw')
-        try:
-            video = await client.download_media(media)
-            await msg.edit("♻️ **converting video...**")
-            os.system(f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" stream-{CHAT_ID}.raw -y')
-        except Exception as e:
-            await msg.edit(f"❌ **something went wrong...** \n`{e}`")
-        await sleep(5)
-        group_call = group_call_factory.get_file_group_call(f'stream-{CHAT_ID}.raw')
-        try:
-            await group_call.start(CHAT_ID)
-            await group_call.set_video_capture(video)
-            VIDEO_CALL[CHAT_ID] = group_call
-            await msg.edit("💡 **video streaming started!**\n\n» **join to video chat to watch the video.**")
-            try:
-                STREAM.remove(0)
-            except:
-                pass
-            try:
-                STREAM.add(1)
-            except:
-                pass
-        except FloodWait as e:
-            await sleep(e.x)
-            if not group_call.is_connected:
-                await group_call.start(CHAT_ID)
-                await group_call.set_video_capture(video)
-                VIDEO_CALL[CHAT_ID] = group_call
-                await msg.edit("💡 **video streaming started!**\n\n» **join to video chat to watch the video.**")
-                try:
-                    STREAM.remove(0)
-                except:
-                    pass
-                try:
-                    STREAM.add(1)
-                except:
-                    pass
-        except Exception as e:
-            await msg.edit(f"❌ **something went wrong...** \nError: `{e}`")
+            await msg.edit(f"**Error** -- `{e}`")
     else:
-        await m.reply_text("🔺 **please reply to a video or youtube link to stream video!**")
-        return
+        await m.reply("🔺 **please reply to a video or live stream url to stream video!**")
 
-
-@Client.on_message(command(["vstop", f"vstop@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
-@authorized_users_only
-async def vstop(client, m: Message):
-    if 0 in STREAM:
-        await m.reply_text("😕 **no active streaming at this time**\n\n» start streaming by using /vstream command (reply to video)")
-        return
+@Client.on_message(filters.command("stop"))
+async def stopvideo(client, m: Message):
+    chat_id = m.chat.id
     try:
-        await VIDEO_CALL[CHAT_ID].stop()
-        await m.reply_text("🔴 **streaming has ended !**\n\n✅ __userbot has been disconnected from the video chat__")
-        try:
-            STREAM.remove(1)
-        except:
-            pass
-        try:
-            STREAM.add(0)
-        except:
-            pass
+        await VIDEO_CALL[chat_id].stop()
+        await m.reply("🔴 **streaming has ended !**\n\n✅ __userbot has been disconnected from the video chat__")
     except Exception as e:
-        await m.reply_text(f"❌ **something went wrong !** \nError: `{e}`")
+        await m.reply(f"**🚫 Error** - `{e}`")
