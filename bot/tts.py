7f2a0e60f02b678e2a86a7b527a8f23753aa9e0f
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-05 10:57:35 +0700

    added tts

---
+++ bot/tts.py
@@ -0,0 +1,44 @@
+# text to speech funtion
+# ported from juvia group bot / module / tts.py
+
+
+import traceback
+from asyncio import get_running_loop
+from io import BytesIO
+
+from googletrans import Translator
+from gtts import gTTS
+from pyrogram import filters
+from pyrogram.types import Message
+
+from bot.videoplayer import app
+
+
+def convert(text):
+    audio = BytesIO()
+    i = Translator().translate(text, dest="en")
+    lang = i.src
+    tts = gTTS(text, lang=lang)
+    audio.name = lang + ".mp3"
+    tts.write_to_fp(audio)
+    return audio
+
+
+@app.on_message(filters.command("tts"))
+async def text_to_speech(_, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("💡 reply to some text...")
+    if not message.reply_to_message.text:
+        return await message.reply_text("💡 reply to some text...")
+    m = await message.reply_text("🔁 processing...")
+    text = message.reply_to_message.text
+    try:
+        loop = get_running_loop()
+        audio = await loop.run_in_executor(None, convert, text)
+        await message.reply_audio(audio)
+        await m.delete()
+        audio.close()
+    except Exception as e:
+        await m.edit(e)
+        e = traceback.format_exc()
+        print(e)
