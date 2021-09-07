c7f85cb57bbec78b94177ed2415b7ee7e0a467e6
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-07 11:46:55 +0700

    fixes
    just a little small update

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -10,19 +10,27 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pytgcalls import GroupCallFactory
 from youtube_dl import YoutubeDL
+from pytube import YouTube
+from youtube_search import YoutubeSearch
 
 from config import API_ID, API_HASH, SESSION_NAME, BOT_USERNAME
 from helpers.decorators import authorized_users_only
 from helpers.filters import command
 
-STREAM = {8}
-VIDEO_CALL = {}
+
+def video_link_getter(url: str):
+    yt = YouTube(url)
+    x = yt.streams.filter(file_extension="mp4", res="720p")[0].download("downloads")
+    return x
 
 ydl_opts = {
     "geo-bypass": True,
     "nocheckcertificate": True,
 }
 ydl = YoutubeDL(ydl_opts)
+
+STREAM = {8}
+VIDEO_CALL = {}
 
 app = Client(
     SESSION_NAME,
@@ -32,51 +40,37 @@ app = Client(
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
 
 
-@Client.on_message(command(["play", f"play@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["vstream", f"vstream@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def startvideo(client, m: Message):
     replied = m.reply_to_message
     if not replied:
         if len(m.command) < 2:
-            await m.reply("💡 reply to video or provide live stream url to start video streaming")
+            await m.reply("💡 reply to video or provide youtube video url to start video streaming")
         else:
-            video = m.text.split(None, 1)[1]
+            video = " ".join(message.command[1:])
             youtube_regex = (
-                                         r'(https?://)?(www\.)?'
-                                       '(youtube|youtu|youtube-nocookie)\.(com|be)/'
-                                       '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+                r'(https?://)?(www\.)?'
+                '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+                '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
             youtube_regex_match = re.match(youtube_regex, video)
             if youtube_regex_match:
-            	try:
-            		yt = pafy.new(video)
-            		best = yt.getbest()
-            		video_url = best.url
-            	except Exception as e:
-            		await m.reply(f"🚫 **error** - `{e}`")
-            		return
-            	msg = await m.reply("🔁 starting video streaming...")
-            	chat_id = m.chat.id
-            	await asyncio.sleep(1)
-            	try:
-            	   group_call = group_call_factory.get_group_call()
-            	   await group_call.join(chat_id)
-            	   await group_call.start_video(video_url, repeat=False)
-            	   VIDEO_CALL[chat_id] = group_call
-            	   await msg.edit((f"💡 **started [video streaming]({video_url}) !\n\n» join to video chat to watch streaming."), disable_web_page_preview=True)
-            	except Exception as e:
-            		await msg.edit(f"🚫 **error** - `{e}`")
-            else:
-            	msg = await m.reply("🔁 starting video streaming...")
-            	chat_id = m.chat.id
-            	await asyncio.sleep(1)
-            	try:
-            	   group_call = group_call_factory.get_group_call()
-            	   await group_call.join(chat_id)
-            	   await group_call.start_video(video, repeat=False)
-            	   VIDEO_CALL[chat_id] = group_call
-            	   await msg.edit((f"💡 **started [video streaming]({video}) !\n\n» join to video chat to watch streaming."), disable_web_page_preview=True)
-            	except Exception as e:
-            		await msg.edit(f"🚫 **error** - `{e}`")
+                try:
+                    x = video_link_getter(video)
+                except Exception as e:
+                    await m.reply(f"**Error** -- `{e}`")
+                    return
+                msg = await m.reply("`Starting Live Stream...`")
+                chat_id = m.chat.id
+                await asyncio.sleep(1)
+                try:
+                    group_call = group_call_factory.get_group_call()
+                    await group_call.join(chat_id)
+                    await group_call.start_video(x, repeat=False)
+                    VIDEO_CALL[chat_id] = group_call
+                    await msg.edit((f"💡 **started [video streaming]({x}) !\n\n» join to video chat on the top to watch streaming."), disable_web_page_preview=True)
+                except Exception as e:
+                    await msg.edit(f"🚫 **error** - `{e}`")
             	
     elif replied.video or replied.document:
         msg = await m.reply("📥 downloading video...")
@@ -95,7 +89,7 @@ async def startvideo(client, m: Message):
         await m.reply("💭 please reply to video or video file to stream")
 
 
-@Client.on_message(command(["end", f"end@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["vstop", f"vstop@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def stopvideo(client, m: Message):
     chat_id = m.chat.id
